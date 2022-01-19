6ee9d673f2a6476b3a98b9f8b6959ad8801d7f69
James-Kelley-Galvanize <james.kelley@galvanize.com>
2022-01-19 13:05:07 -0700

    Finish Tree

--- Tree.js
+++ Tree.js
@@ -1,16 +1,29 @@
 var Tree = function (value) {
   var newTree = {};
   newTree.value = value;
+  Object.assign(newTree, treeMethods);
 
-  // your code here
-  newTree.children = null; // fix me
+  newTree.children = [];
 
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function (value) {};
+treeMethods.addChild = function (value) {
+  let newChild = Tree(value);
+  this.children.push(newChild);
+};
 
-treeMethods.contains = function (target) {};
+treeMethods.contains = function (target) {
+  // check if this node has the target value
+  if (this.value === target) return true;
+  // if not, does this have children
+  if (this.children) {
+    return this.children.some((childNode) => childNode.contains(target));
+  }
+  return false;
+  // if yes, check the children
+  // if no, return false
+};
 module.exports = Tree;
