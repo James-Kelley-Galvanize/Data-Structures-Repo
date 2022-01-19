c5940bd7c1fe4771cb86b7ae18c6254b660fd0bc
James-Kelley-Galvanize <james.kelley@galvanize.com>
2022-01-19 16:01:22 -0700

    Finish MOST of HashTable

--- HashTable.js
+++ HashTable.js
@@ -55,15 +55,18 @@ var HashTable = function () {
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function (k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function (key, value) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  this._storage.set(index, value);
 };
 
 HashTable.prototype.retrieve = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index);
 };
 module.exports = HashTable;
